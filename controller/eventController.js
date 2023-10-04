3470de33b889d65c9fc4cd4804547a3ca1c18c2f
arash ramy <arashramy@gmail.com>
2023-10-04 12:35:41 +0330

    finishing sql server sidevar crud

--- controller/eventController.js
+++ controller/eventController.js
@@ -21,12 +21,14 @@ router.post(
   "/create",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const {caption,row,floor}=req.body;
-      await sql
-        .connect(config.sql)
-        .then(async () => {
-          let connection = new sql.Request();
-          let result = await connection.query(`USE ARASH    
+      const { caption, row, floor } = req.body;
+      await sql.connect(config.sql).then(async () => {
+        let connection = new sql.Request();
+        let result = await connection.query(`USE ARASH   
+        UPDATE Sidebar
+        SET Row = Row +1 WHERE Row IN (SELECT Row FROM Sidebar
+        WHERE  Row  >= '${req.body.Row}' )
+
           INSERT INTO [dbo].[Sidebar]
                      (
                     [Caption]
@@ -37,7 +39,7 @@ router.post(
                      (
                      '${req.body.Caption}',
                      '${req.body.Row}',
-                     '${req.body.Floor}',
+                     '${req.body.floor}',
                      '${req.body.ParentId}'
                 
           
@@ -45,11 +47,10 @@ router.post(
           
           
           `);
-          console.log(result.recordset);
-          const data=result.recordset
-          return res.json({data})
-        })
-        
+        console.log(result.recordset);
+        const data = result.recordset;
+        return res.json({ data });
+      });
 
       // const events = eventData.getEvents();
       //   console.log(events,"d")
@@ -59,30 +60,49 @@ router.post(
     }
   })
 );
-
-
-
-
 
 router.delete(
   "/delete",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const {caption,Row,floor}=req.body;
-      await sql
-        .connect(config.sql)
-        .then(async () => {
-          let connection = new sql.Request();
-          let result = await connection.query(`USE [ARASH]    
-          DELETE FROM [dbo].[Sidebar]
-                WHERE Row='${req.body.Row}'
+      const { caption, Row, floor } = req.body;
+      await sql.connect(config.sql).then(async () => {
+        let connection = new sql.Request();
+        let result = await connection.query(`USE [ARASH]    
+        DELETE FROM [dbo].[Sidebar]
+              WHERE Row= '${req.body.Row}' AND Floor= '${req.body.floor}'
          
-          `);
-          console.log(result.recordset);
-          const data=result.recordset
-          return res.json({data})
-        })
-        
+        UPDATE Sidebar
+        SET Row = Row -1 WHERE Row IN (SELECT Row FROM Sidebar
+        WHERE  Row  > '${req.body.Row}')  ; `);
+        console.log(result.recordset);
+        const data = result.recordset;
+        return res.json({ data });
+      });
+
+      // const events = eventData.getEvents();
+      //   console.log(events,"d")
+      // res.json(events)
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  })
+);
+
+router.get(
+  "/getAllSidebar",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      console.log("get");
+
+      const { caption, row, floor } = req.body;
+      await sql.connect(config.sql).then(async () => {
+        let connection = new sql.Request();
+        let result = await connection.query(`USE [ARASH]  SELECT * FROM Sidebar`);
+        console.log(result.recordsets);
+        const data = result.recordset;
+        return res.json({ data });
+      });
 
       // const events = eventData.getEvents();
       //   console.log(events,"d")
@@ -97,67 +117,34 @@ router.delete(
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.post(
-  "/create",
+router.patch(
+  "/update",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const {caption,row,floor}=req.body;
-      const dataInsert={
-        caption,
-        row,
-        floor
-      }
-      console.log(req.body)
+      const { caption, row, floor ,newFloor, newCaption ,newRow } = req.body;
+   
+      console.log(req.body);
       await sql
         .connect(config.sql)
         .then(async () => {
           let connection = new sql.Request();
-          let result = await connection.query(` INSERT INTO sidebar (sidebarCaption ,row , floor  )
-          VALUES(${
-            caption
-          },${
-            row
-          },
-          ${
-            floor
-          }
- 
- 
-          `) 
+          let result =
+            await connection.query(` USE [ARASH] 
+            UPDATE Sidebar
+            SET Row = '${req.body.newRow}'
+            WHERE  Row = '${req.body.Row}' AND FLOOR='${req.body.floor}'  
+              
+            UPDATE Sidebar
+            SET Row = '${req.body.Row}'
+            WHERE  Row = '${req.body.newRow}' AND FLOOR='${req.body.floor}' AND  Caption='${req.body.Caption}' 
+          
+           
+          
+          `);
           //body: {row: 0, floor: 1} => VALUES({row: 0, floor: 1} , 'this is text for event Description 4 ' , '2023-12-05','2023-12-12' ,'shamsaei',1000)
           // console.log(result.recordset);
-          const data=result.recordset
-          return res.json({data})
+          const data = result.recordset;
+          return res.json({ data });
         })
         .then(async (resp) => {
           res.json({ resp });
@@ -177,4 +164,155 @@ router.post(
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get(
+  "/testi",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { caption, row, floor ,newFloor, newCaption ,newRow } = req.body;
+   
+      console.log(req.body);
+      await sql
+        .connect(config.sql)
+        .then(async () => {
+          let connection = new sql.Request();
+          let result =
+            await connection.query(` 
+
+          
+            USE [ARASH] 
+SELECT AVG( Row  )
+FROM Sidebar;
+          `);
+          //body: {row: 0, floor: 1} => VALUES({row: 0, floor: 1} , 'this is text for event Description 4 ' , '2023-12-05','2023-12-12' ,'shamsaei',1000)
+          // console.log(result.recordset);
+          console.log("run then sec 1")
+
+          const data = result.recordset;
+          return res.json({ data });
+        })
+        // .then(async (resp) => {
+        //   console.log("run then sec 2")
+        //   return  res.json({ resp });
+        // });
+
+      // const events = eventData.getEvents();
+      //   console.log(events,"d")
+      // res.json(events)
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  })
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post(
+//   "/create",
+//   catchAsyncErrors(async (req, res, next) => {
+//     try {
+//       const { caption, row, floor } = req.body;
+//       const dataInsert = {
+//         caption,
+//         row,
+//         floor,
+//       };
+//       console.log(req.body);
+//       await sql
+//         .connect(config.sql)
+//         .then(async () => {
+//           let connection = new sql.Request();
+//           let result =
+//             await connection.query(` INSERT INTO sidebar (sidebarCaption ,row , floor  )
+//           VALUES(${caption},${row},
+//           ${floor}
+ 
+ 
+//           `);
+//           //body: {row: 0, floor: 1} => VALUES({row: 0, floor: 1} , 'this is text for event Description 4 ' , '2023-12-05','2023-12-12' ,'shamsaei',1000)
+//           // console.log(result.recordset);
+//           const data = result.recordset;
+//           return res.json({ data });
+//         })
+//         .then(async (resp) => {
+//           res.json({ resp });
+//         });
+
+//       // const events = eventData.getEvents();
+//       //   console.log(events,"d")
+//       // res.json(events)
+//     } catch (error) {
+//       res.status(400).send(error.message);
+//     }
+//   })
+// );
